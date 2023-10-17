29d16aeb5600a864502696ab5d509feb205dd0ef
kouwa stephane <kksasteph888@gmail.com>
2023-10-17 12:05:33 +0200

    dev logs request

--- src/EventSubscriber/SemaphoreSubscriber.php
+++ src/EventSubscriber/SemaphoreSubscriber.php
@@ -12,52 +12,61 @@ use Symfony\Component\HttpKernel\KernelEvents;
  * semaphore event subscriber.
  */
 class SemaphoreSubscriber implements EventSubscriberInterface {
-
+  
   /**
    * The messenger.
    *
    * @var \Drupal\Core\Messenger\MessengerInterface
    */
   protected $messenger;
-
+  
   /**
    * Constructs event subscriber.
    *
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
-   *   The messenger.
+   *        The messenger.
    */
   public function __construct(MessengerInterface $messenger) {
     $this->messenger = $messenger;
   }
-
+  
   /**
    * Kernel request event handler.
    *
    * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
-   *   Response event.
+   *        Response event.
    */
   public function onKernelRequest(GetResponseEvent $event) {
-    $this->messenger->addStatus(__FUNCTION__);
+    // $this->messenger->addStatus(__FUNCTION__);
+    $request = $event->getRequest();
+    \Stephane888\Debug\debugLog::kintDebugDrupal($request, $request->getMethod() . '--onKernelRequest', true, 'logs/request');
   }
-
+  
   /**
    * Kernel response event handler.
    *
    * @param \Symfony\Component\HttpKernel\Event\FilterResponseEvent $event
-   *   Response event.
+   *        Response event.
    */
   public function onKernelResponse(FilterResponseEvent $event) {
-    $this->messenger->addStatus(__FUNCTION__);
+    // $this->messenger->addStatus(__FUNCTION__);
+    // \Stephane888\Debug\debugLog::kintDebugDrupal($event, 'onKernelResponse',
+    // true, 'logs/reponse');
   }
-
+  
   /**
+   *
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
     return [
-      KernelEvents::REQUEST => ['onKernelRequest'],
-      KernelEvents::RESPONSE => ['onKernelResponse'],
+      KernelEvents::REQUEST => [
+        'onKernelRequest'
+      ],
+      KernelEvents::RESPONSE => [
+        'onKernelResponse'
+      ]
     ];
   }
-
+  
 }
